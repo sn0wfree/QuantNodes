0ea06881d1bdaa8e1f921fca931f68994aa4e760
sn0wfree <snowfreedom0815@gmail.com>
2020-11-10 20:33:59 +0800

    add to method to update function

--- Nodes/Factor/ch_factor.py
+++ Nodes/Factor/ch_factor.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import copy
 import re
 from collections import namedtuple, ChainMap
 
@@ -6,9 +7,6 @@ from ClickSQL import ClickHouseTableNodeExt
 
 factor_parameters = ('dt', 'code', 'value', 'fid')
 ft_node = namedtuple('factortable', factor_parameters)
-
-
-# from Nodes.utils_node.timer import timer
 
 
 class FactorBackendCH(object):
@@ -19,17 +17,9 @@ class FactorBackendCH(object):
 
         :param src:  sample: clickhouse://test:sysy@199.199.199.199:1234/drre
         """
-        # db_settings = parse_rfc1738_args(src)
         self._src = src
-        # self._settings = db_settings
-        # self._store_type = self._settings['name']
-
-        # self._para = node(self._settings['host'], self._settings['port'], self._settings['user'],
-        #                   self._settings['password'], self._settings['database'])  # store connection information
         self.node = ClickHouseTableNodeExt(conn_str=src)
         self.db_table = self.node._para.database
-        # db_settings['host'], db_settings['port'], db_settings['user'],
-        # db_settings['password'], db_settings['database']
 
     def __call__(self, sql: str, **kwargs):
         return self.node.query(sql, **kwargs)
@@ -74,19 +64,16 @@ class FactorBackendCH(object):
             cols = factor_parameters
         elif len(cols) == 0:
             cols = ['*']
-
         conditions = ChainMap(data_filter, *list(cls.__obtain_other_filter__(other_filters)))
         filter_yield = cls.__extend_dict_value__(conditions)
         if include_filter:
             cols = set(list(cols) + list(conditions.keys()))
         else:
             cols = set(cols)
-
         sql = f"select {','.join(cols)} from {db_table} where {' and '.join(sorted(set(['1'] + list(filter_yield))))} "
         return sql
 
     def _execute(self, sql: str, **kwargs):
-
         return self.node.query(sql, **kwargs)
         # self.__execute__ = self.operator.query
 
@@ -99,17 +86,10 @@ class FactorBackendCH(object):
             return True
 
 
-#
-# def EXECUTE(obj, sql: str):
-#     if hasattr(obj, '__execute__'):
-#         return obj.__execute__(sql)
-#     else:
-#         raise ValueError()
-
-
-class BaseSingleFactorHelper(object):
+class BaseSingleFactorNode(object):
     __Name__ = "基础因子库单因子基类"
-    __slots__ = ('_operator', 'db', 'table', 'db_table', '_kwargs', '_raw_kwargs', '_status', '_INFO', 'depend_tables')
+    __slots__ = (
+        'operator', 'db', 'table', 'db_table', '_kwargs', '_raw_kwargs', 'status', '_INFO', 'depend_tables')
 
     def __init__(self, src: str, db_table: (None, str) = None, info=None, **kwargs):
         """
@@ -121,12 +101,12 @@ class BaseSingleFactorHelper(object):
         :param kwargs:  data_filter will store operator for some cols
         """
 
-        self._operator = FactorBackendCH(src)
+        self.operator = FactorBackendCH(src)
 
         # self._execute = self._operator._execute
 
         if db_table is None:
-            src_db_table = self._operator.db_table
+            src_db_table = self.operator.db_table
             if '.' in src_db_table:
                 self.db_table = src_db_table
             else:
@@ -141,43 +121,62 @@ class BaseSingleFactorHelper(object):
         self.depend_tables = [self.db_table]
         self._kwargs = kwargs
         self._raw_kwargs = kwargs
-        self._status = 'SQL'
+        self.status = 'SQL'
         self._INFO = info
 
-    @property
-    def status(self):
-        return self._status
-
-    @status.setter
-    def status(self, status: str):
-        self._status = status
+    def update(self, **kwargs):
+        self._kwargs.update(kwargs)
 
     def __str__(self):
         return self.__sql__
 
     @property
     def __sql__(self):
-        return self._operator._get_sql(db_table=self.db_table, **self._kwargs)
+        return self.operator._get_sql(db_table=self.db_table, **self._kwargs)
 
     @property
-    def row_count(self):
-        sql = f"select count(1) as row_count from {self.db_table}"
-        row_count = self._operator(sql)['row_count'].values[0]
+    def __factor_id__(self):  # add iid function get factor table id
+        return hash(self.__sql__)
+
+    @property
+    def total_rows(self):
+        """
+        return row count
+        :return:
+        """
+
+        # sql = f"-- select count(1) as row_count from {self.db_table}"
+        row_count = self.__system_tables__['total_rows'].values[0]
         return row_count
 
     @property
+    def __system_tables__(self):
+        sql = f"select total_rows,engine from system.tables where database ='{self.db}' and name='{self.table}'"
+        res = self.operator(sql)
+        return res
+
+    @property
+    def table_exist(self):
+        """
+        return table exists status
+        :return:
+        """
+
+        return not self.__system_tables__.empty
+
+    @property
     def table_engine(self):
-        sql = f"select engine from system.tables where database ='{self.db}' and name='{self.table}'"
-        engine = self._operator(sql)['engine'].values[0]
-        return engine
+        """
+        return table engine
+        :return:
+        """
+
+        return self.__system_tables__['engine'].values[0]
 
     @property
     # @timer
     def empty(self):
-        if self.row_count == 0:
-            return True
-        else:
-            return False
+        return self.total_rows == 0
 
     def fetch(self, pattern=r'[\s]+limit[\s]+[0-9]+$'):
         """
@@ -185,11 +184,11 @@ class BaseSingleFactorHelper(object):
         :return:
         """
         sql = self.__sql__
-        end_with_limit = self._operator._check_end_with_limit(sql, pattern=pattern)
+        end_with_limit = self.operator._check_end_with_limit(sql, pattern=pattern)
         if end_with_limit:
-            return self._operator(sql)
+            return self.operator(sql)
         else:
-            return self._operator(sql + ' limit 1000')
+            return self.operator(sql + ' limit 1000')
 
     def fetch_all(self):
         """
@@ -197,10 +196,7 @@ class BaseSingleFactorHelper(object):
         :return:
         """
 
-        return self._operator(self.__sql__)
-
-    def update(self, **kwargs):
-        self._kwargs.update(kwargs)
+        return self.operator(self.__sql__)
 
     def __call__(self, **kwargs):
         """
@@ -209,55 +205,30 @@ class BaseSingleFactorHelper(object):
         :return:
         """
         self.update(**kwargs)
-        if self._status == 'SQL':
+        if self.status == 'SQL':
             return self.__sql__
-        elif self._status == 'SQL:fetch':
+        elif self.status == 'SQL:fetch':
             return self.fetch()
-        elif self._status == 'SQL:fetch_all':
+        elif self.status == 'SQL:fetch_all':
             return self.fetch_all()
-
-    # def __data__(self):
-    #     return self.__query__(**self._kwargs)
-    @property
-    def operator(self):
-        return self._operator
-
-
-class BaseSingleFactorTableNode(BaseSingleFactorHelper):
-    pass
-
-
-"add auto-increment col by materialized bitOr(bitShiftLeft(toUInt64(now64()),24), rowNumberInAllBlocks()) "
-
-
-class UpdateProcess(object):
-    @classmethod
-    def update(cls, src_table: BaseSingleFactorTableNode,
-               dst_table: BaseSingleFactorTableNode,
-               fid_ck: str = 'fid',
-               dt_max_1st=True):
-        if src_table.empty:
-            raise ValueError(f'{src_table.db_table} is empty')
-        if src_table.empty:
-            update_status = 'full'
-            sql = cls.full_update(src_table, dst_table)
         else:
-            update_status = ' incremental'
-            sql = cls.incremental_update(src_table, dst_table, fid_ck, dt_max_1st=dt_max_1st)
-        return sql, update_status
+            raise ValueError('status code is not supported!')
+
+
+class UpdateSQLUtils(object):
 
     @staticmethod
-    def full_update(src_table: BaseSingleFactorTableNode, dst_table: BaseSingleFactorTableNode):
-        dst_db_table = dst_table.db_table
-        insert_sql = f"insert into {dst_db_table} {src_table}"
+    def full_update(src_db_table: BaseSingleFactorNode, dst_db_table: BaseSingleFactorNode, **kwargs):
+        dst_db_table = dst_db_table.db_table
+        insert_sql = f"insert into {dst_db_table} {src_db_table}"
         return insert_sql
 
     @staticmethod
-    def incremental_update(src_table: BaseSingleFactorTableNode, dst_table: BaseSingleFactorTableNode,
-                           fid_ck: str, dt_max_1st=True):
+    def incremental_update(src_db_table: BaseSingleFactorNode, dst_db_table: BaseSingleFactorNode,
+                           fid_ck: str, dt_max_1st=True, **kwargs):
         # src_db_table = src_table.db_table
-        src_table_type = src_table.table_engine
-        dst_db_table = dst_table.db_table
+        src_table_type = src_db_table.table_engine
+        dst_db_table = dst_db_table.db_table
         if dt_max_1st:
             order_asc = ' desc'
         else:
@@ -266,30 +237,79 @@ class UpdateProcess(object):
             sql = f'select max({fid_ck}) as {fid_ck} from {dst_db_table}'
         else:
             sql = f" select distinct {fid_ck} from {dst_db_table} order by {fid_ck} {order_asc} limit 1 "
-        fid_ck_values = dst_table.operator(sql).values[0]
-        src_table.update(**{f'{fid_ck} as src_{fid_ck}': f' {fid_ck} > {fid_ck_values}'})
+        fid_ck_values = dst_db_table.operator(sql).values[0]
+        src_db_table.update(**{f'{fid_ck} as src_{fid_ck}': f' {fid_ck} > {fid_ck_values}'})
 
-        insert_sql = f"insert into {dst_db_table} {src_table}"
+        insert_sql = f"insert into {dst_db_table} {src_db_table}"
         return insert_sql
 
-        # start, end = dt_ck_df.min(), dt_ck_df.max()
 
-        # insert_sql = f"insert into {dst_db_table} {src_table}"
-        # return insert_sql
+class BaseSingleFactorTableNode(BaseSingleFactorNode):
+    # update table
+    def to(self,
+           dst_db_table: (BaseSingleFactorNode, str),
+           fid_ck: str = 'fid',
+           dt_max_1st: bool = True,
+           execute: bool = True,
+           no_self_update: bool = True):
+        """
 
+        UpdateSQLUtils
+
+        :param dst_db_table:
+        :param fid_ck:
+        :param dt_max_1st:
+        :param execute:
+        :param no_self_update:
+        :return:
+        """
+        if self.empty:
+            raise ValueError(f'{self.db_table} is empty')
+
+        if isinstance(dst_db_table, str):
+            dst_conn = copy.deepcopy(self.operator._src)
+            dst_db_table = BaseSingleFactorNode(
+                dst_conn.replace(self.db_table, dst_db_table),
+                cols=['*']
+            )
+        elif isinstance(dst_db_table, BaseSingleFactorNode):
+            pass
+        else:
+            raise ValueError('dst_db_table is not valid! please check!')
+        # check two table are same
+        if no_self_update and self.db_table == dst_db_table.db_table:
+            if self.__factor_id__ == dst_db_table.__factor_id__:
+                dst = dst_db_table.db_table
+                src = self.db_table
+                raise ValueError(
+                    f'Detect self-update process! these operator attempts to update data from {src} to {dst}')
+
+        update_status = 'full' if dst_db_table.empty else 'incremental'
+
+        func = getattr(UpdateSQLUtils, f'{update_status}_update')
+        sql = func(self, dst_db_table, fid_ck, dt_max_1st=dt_max_1st)
+        if execute:
+            self.operator(sql)
+        return sql, update_status
+
+
+"add auto-increment col by materialized bitOr(bitShiftLeft(toUInt64(now64()),24), rowNumberInAllBlocks()) "
 
 # create table
-# update table
+
 
 # merge table
 
 # group table
 if __name__ == '__main__':
-    factor = BaseSingleFactorHelper(
-        'clickhouse://default:***REMOVED***@***REMOVED***:8123/raw.product_id_info',
+    factor = BaseSingleFactorNode(
+        'clickhouse://default:***REMOVED***@***REMOVED***:8123/raws.std',
         cols=['test1']
     )
-    print(factor.empty)
-    print(factor.table_engine)
-    print(f"{factor}")
+    # print(factor.empty)
+    # print(factor.table_engine)
+    # print(f"{factor}")
+    # c = hash('sdhushdu'.replace('s', 't'))
+    # print('sdhushdu'.replace('s', 't'))
+    print(factor.table_exists)
     pass
