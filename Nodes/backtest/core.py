7d085c90a80ff317d1076c70d7f75a60c5c77589
sn0wfree <snowfreedom0815@gmail.com>
2020-12-23 16:49:25 +0800

    add backtest

--- Nodes/backtest/core.py
+++ Nodes/backtest/core.py
@@ -1,22 +1,12 @@
 # coding=utf-8
-from abc import ABCMeta, abstractmethod
-from collections import Iterable
-import multiprocessing as mp
-import os
-import sys
-import warnings
 from abc import abstractmethod, ABCMeta
-
-from copy import copy
-from functools import lru_cache, partial
-from itertools import repeat, product, chain, compress
-from math import copysign
-from numbers import Number
-from typing import Callable, Dict, List, Optional, Sequence, Tuple, Type, Union
-import numpy as np
-import pandas as pd
+from collections import Iterable
 from collections import OrderedDict, namedtuple
+from Nodes.test import GOOG
 import datetime
+import pandas as pd
+from functools import lru_cache
+import numpy as np
 
 OHLCV_AGG = OrderedDict((
     ('Open', 'first'),
@@ -48,7 +38,7 @@ class QuoteData(object):
         self._end = end
         if target_cols is None:
             target_cols = list(OHLCV_AGG.keys())
-        self._data = df[(df[date] >= self._start) and (df[date] <= self._end)].sort_values(date, ascending=True)
+        self._data = df[(df[date] >= self._start) & (df[date] <= self._end)].sort_values(date, ascending=True)
         self.target_cols = target_cols
 
         self._length = len(self._data)
@@ -59,6 +49,12 @@ class QuoteData(object):
             raise AttributeError(f'Column {code} or {date} not in data')
 
         self._setup()
+
+    def shape(self):
+        return self._data
+
+    def length(self):
+        return len(self._data[self._general_cols[0]].unique())
 
     def __len__(self):
         return self._length
@@ -147,21 +143,6 @@ class Broker(object):
         self.trades = []
         self.closed_trades = []
 
-    @staticmethod
-    def set_order(size: float,
-                  limit_price: float = None,
-                  stop_price: float = None,
-                  sl_price: float = None,
-                  tp_price: float = None,
-                  order_id=None,
-                  parent_trade=None):
-        return Orders(size,
-                      limit_price=limit_price,
-                      stop_price=stop_price,
-                      sl_price=sl_price,
-                      tp_price=tp_price,
-                      order_id=order_id,
-                      parent_trade=parent_trade)
         pass
 
     def sell(self, orders):
@@ -196,12 +177,51 @@ class BackTest(object):
 
 
 class Orders(object):
+    @staticmethod
+    def set_order(size: float,
+                  limit_price: float = None,
+                  stop_price: float = None,
+                  sl_price: float = None,
+                  tp_price: float = None,
+                  order_id=None,
+                  parent_trade=None):
+        return Order(size,
+                     limit_price=limit_price,
+                     stop_price=stop_price,
+                     sl_price=sl_price,
+                     tp_price=tp_price,
+                     order_id=order_id,
+                     parent_trade=parent_trade)
+
+    @classmethod
+    def create_orders(cls, scripts,
+
+                      default_limit=None,
+                      default_stop=-np.inf, default_sl=-np.inf,
+                      default_tp=np.inf):
+        default_dict = {'limit': default_limit, 'stop': default_stop, 'sl': default_sl, 'tp': default_tp}
+        cols = scripts.columns
+        must = ['date', 'code', 'size']
+        reqired = ['limit', 'stop', 'sl', 'tp']
+        missed = set(must) - set(cols)
+        if len(missed) != 0:
+            raise ValueError(f"{','.join(missed)} are missing")
+
+        for c in reqired:
+            if c in cols:
+                pass
+            else:
+                scripts[c] = default_dict[c]
+
+
+class Order(object):
     """
     订单执行系统
     """
 
     def __init__(self,
                  size: float,
+                 code: str,
                  limit_price: float = None,
                  stop_price: float = None,
                  sl_price: float = None,
@@ -209,24 +229,53 @@ class Orders(object):
                  order_id=None,
                  create_date=None,
                  parent_trade=None):
-        ORDERSCreator = namedtuple("ORDER", ('order_id', 'size', 'limit_price', 'stop_price', 'sl_price', 'tp_price'))
+        ORDERSCreator = namedtuple("ORDER",
+                                   ('order_id', 'code', 'size', 'limit_price', 'stop_price', 'sl_price', 'tp_price'))
         self.create_date = create_date
         self._order_id = _order_id = order_id if order_id is not None else 'order_' + random_str(num=19)
         self._parent_trade = parent_trade
-        self._attr = ORDERSCreator(_order_id, size, limit_price, stop_price, sl_price, tp_price)
+        self._attr = ORDERSCreator(_order_id, code, size, limit_price, stop_price, sl_price, tp_price)
 
     def __repr__(self):
         attr = (('order_id', self._order_id),
+                ('code', self._attr.code),
                 ('size', self._attr.size),
-                ('limit', self._attr.limit_price),
-                ('stop', self._attr.stop_price),
-                ('sl', self._attr.sl_price),
-                ('tp', self._attr.tp_price),
-                ('create_date', self.create_date),
-                )
+                ('limit', self._attr.limit_price),  # 限价单或者市场单
+                ('stop', self._attr.stop_price),  # 止盈止损市场单
+                ('sl', self._attr.sl_price),  # 止损限价单
+                ('tp', self._attr.tp_price),  # 止盈限价单
+                ('create_date', self.create_date))
         settings = ','.join([f'{name}={value}' for name, value in attr])
 
         return f'<Order {settings}>'
+
+
+    def _adjusted_price(self,size, commission, size, price) -> float:
+        """
+        Long/short `price`, adjusted for commisions.
+        In long positions, the adjusted price is a fraction higher, and vice versa.
+        """
+        return (price or self.last_price) * (1 + copysign(self._commission, size))
+
+    def oper(self, quotedata):
+        code_data = quotedata[quotedata['code'] == self._attr.code]
+
+        """
+        default_stop=-np.inf, default_sl=-np.inf,default_tp=np.inf
+        """
+
+        if self.is_long:
+
+            if not (self.sl_price or -np.inf) < (self.limit_price or self.stop_price or adjusted_price) < (self.tp_price or np.inf):
+                raise ValueError(
+                    "Long orders require: "
+                    f"SL ({self.sl_price}) < LIMIT ({self.limit_price or self.stop_price or adjusted_price}) < TP ({self.tp_price})")
+        else:
+            if not (self.tp_price or -np.inf) < (self.limit_price or self.stop_price or adjusted_price) < (self.sl_price or np.inf):
+                raise ValueError(
+                    "Short orders require: "
+                    f"TP ({self.tp_price}) < LIMIT ({self.limit_price or self.stop_price or adjusted_price}) < SL ({self.sl_price})")
+
 
     @property
     def size(self):
@@ -274,12 +323,32 @@ class Indicators(object):
 
 
 if __name__ == '__main__':
+    import numpy as np
+
+    # GOOG = GOOG.reset_index().rename(columns={'index': 'date'})
+    # GOOG['Code'] = 'GOOG'
+
+    # QD = QuoteData(GOOG)
+    # print(len(QD))
+    price = pd.DataFrame(np.random.random(size=(GOOG.shape[0], 1)), columns=['GOOG'])
+    orders = (price > 0.5) * 1
+    price['date'] = GOOG.index
+    orders['date'] = GOOG.index
+    scripts = orders.set_index('date').stack().reset_index()
+    scripts.columns = ['date', 'code', 'size']
     from Nodes.test import GOOG
 
-    GOOG = GOOG.reset_index().rename(columns={'index': 'date'})
-    GOOG['Code'] = 'GOOG'
-
+    ['size', 'limit', 'stop', 'sl', 'tp']
     QD = QuoteData(GOOG)
-    for dt, s in iter(QD):
-        print(dt)
+
+    "A limit order is a type of order to purchase or sell a security at a specified price or better. " \
+    "For buy limit orders, the order will be executed only at the limit price or a lower one, " \
+    "while for sell limit orders, the order will be executed only at the limit price or a higher one. " \
+    "This stipulation allows traders to better control the prices they trade."
+
+    # class BuyandHoldStrategy(Strategy):
+    #     def init(self, scripts, cols=['date', 'code', 'size']):
+    #         code_list = scripts['code'].unique().tolist()
     pass
+
+pass
